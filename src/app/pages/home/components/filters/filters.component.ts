2e0636fdb8de13ae4348b7c4f7803660b0941d27
Ayoub <ayoubaouinti1990@gmail.com>
2023-05-11 23:56:20 +0000

    seven commit

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-filters',
   templateUrl: 'filters.component.html',
 })
-export class FiltersComponent {
+export class FiltersComponent implements OnInit {
+ 
+  categories = ['shoes','sports'];
+  
+  constructor() {}
+
+
+  ngOnInit(): void {
+    
+  }
 
 }
