c6d28e1ac839b02f708b9685088adf0c9fe4864c
Ayoub <ayoubaouinti1990@gmail.com>
2023-05-13 15:50:51 +0000

    tenth commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,25 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+const ROWS_HEIGHT: { [id:number]: number} = { 1: 400, 3:335, 4:350 };
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
   cols = 3;
+  rowHeight = ROWS_HEIGHT[this.cols];
+  category: string | undefined;
 
-  constructor() { /* TODO document why this constructor is empty */ }
+  constructor() { }
 
   ngOnInit(): void {
-    // TODO document why this method 'ngOnInit' is empty
-      
+       
   }
 
   onColumnsCountChange(colsNum: number): void{
     this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
   }
 
-  onShowCategory() {
-
+  onShowCategory(newCategory: string) {
+    this.category = newCategory
   }
 
 }
